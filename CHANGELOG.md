6add1f08919866d45ef92cc9f3954277abef9619
VadimUkolov <chatme@posteo.net>
2023-01-24 14:53:34 +0600

    initial release

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Changelog
+
+## [1.0.0] - 2023-01-24
+This is the first release of Red Bjorn utilities for Unity engine
---
+++ Runtime/Log/Logger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace RedBjorn.Utils
+{
+    public abstract class Logger : ILogger
+    {
+        public string Prefix;
+
+        public void SetPrefix(string prefix)
+        {
+            Prefix = prefix;
+        }
+
+        public void Info(object message)
+        {
+            Debug.Log(Prefix + message);
+        }
+
+        public void Warning(object message)
+        {
+            Debug.LogWarning(Prefix + message);
+        }
+
+        public void Error(object message)
+        {
+            Debug.LogError(Prefix + message);
+        }
+    }
+}
