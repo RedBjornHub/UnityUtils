d27d54058fcb640e5ec014c2bf64ac08f8594f84
VadimUkolov <chatme@posteo.net>
2023-01-31 17:27:14 +0600

    Add Logger Enabled parameters

--- Runtime/Log/Logger.cs
+++ Runtime/Log/Logger.cs
@@ -2,28 +2,46 @@
 
 namespace RedBjorn.Utils
 {
-    public abstract class Logger : ILogger
+    /// <summary>
+    /// RedBjorn.Utils.ILogger implementation which wraps UnityEngine.Debug.Log with customizable enable/disable logic
+    /// </summary>
+    public abstract class Logger : RedBjorn.Utils.ILogger
     {
-        public string Prefix;
+        string Prefix;
+        bool InfoEnabled;
+        bool WarningEnabled;
+        bool ErrorEnabled;
 
         public void Init(string prefix, bool infoEnabled, bool warningEnabled, bool errorEnabled)
         {
             Prefix = prefix;
-        }
-
-        public void Info(object message)
-        {
-            Debug.Log(Prefix + message);
-        }
-
-        public void Warning(object message)
-        {
-            Debug.LogWarning(Prefix + message);
+            InfoEnabled = infoEnabled;
+            WarningEnabled = warningEnabled;
+            ErrorEnabled = errorEnabled;
         }
 
         public void Error(object message)
         {
-            Debug.LogError(Prefix + message);
+            if (ErrorEnabled)
+            {
+                Debug.LogError(Prefix + message);
+            }
+        }
+
+        public void Info(object message)
+        {
+            if (InfoEnabled)
+            {
+                Debug.Log(Prefix + message);
+            }
+        }
+
+        public void Warning(object message)
+        {
+            if (WarningEnabled)
+            {
+                Debug.LogWarning(Prefix + message);
+            }
         }
     }
 }
